4587f78cc2a58eb3bc7dee216f7aed9e381d9895
hems354 <1094617242@qq.com>
2021-12-16 22:20:23 +0800

    Create test10_2.py

---
+++ test10_2.py
@@ -0,0 +1,28 @@
+'''
+MLP的简洁代码实现
+data：2021/12/16
+'''
+import torch
+from torch import nn
+from d2l import torch as d2l
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(device)
+#定义模型
+net = nn.Sequential(nn.Flatten(),
+                    nn.Linear(784, 256),
+                    nn.ReLU(),
+                    nn.Linear(256, 10))
+
+def init_weights(m):
+    if type(m) == nn.Linear:
+        nn.init.normal_(m.weight, std=0.01)
+
+net.apply(init_weights);
+
+batch_size, lr, num_epochs = 256, 0.1, 10
+loss = nn.CrossEntropyLoss()
+trainer = torch.optim.SGD(net.parameters(), lr=lr)
+
+train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size)
+d2l.train_ch3(net, train_iter, test_iter, loss, num_epochs, trainer)
